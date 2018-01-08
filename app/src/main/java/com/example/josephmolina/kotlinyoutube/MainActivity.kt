2f4b6db5bc98a656db77e7910832ad14a2156182
Joseph Molina <jomolina@csumb.edu>
2018-01-07 19:21:04 -0800

    Added OkHttp client

--- app/src/main/java/com/example/josephmolina/kotlinyoutube/MainActivity.kt
+++ app/src/main/java/com/example/josephmolina/kotlinyoutube/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.josephmolina.kotlinyoutube
 
+import android.app.DownloadManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*;
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +16,25 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView_main.layoutManager = LinearLayoutManager(this);
         recyclerView_main.adapter = MainAdapter()
+
+        fetchJson()
+    }
+
+    private fun fetchJson() {
+        println("Attempting to print out json")
+        val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onResponse(call: Call?, response: Response?) {
+                val body = response?.body()?.string()
+                println(body)
+            }
+
+            override fun onFailure(call: Call?, e: IOException?) {
+                println("Failed to execute request")
+            }
+
+        })
     }
 }
